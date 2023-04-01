77360bee7164e83b6b0560ad1175cc84372de7f5
郭桓桓 <hi@nekohuan.cyou>
2023-04-01 22:04:20 +0800

    refactor(index.ts): improve program logic

--- index.ts
+++ index.ts
@@ -15,24 +15,18 @@ const converter = OpenCC.Converter({ from: 'cn', to: 'tw' })
 
 let from: string
 
-if (data.from === '' && data.from_who === '') {
-  from = '\n ——佚名'
-} else if (data.from !== '') {
-  from = `\n ——${converter(data.from)}`
-} else if (data.from_who !== '') {
-  from = `\n ——${converter(data.from_who)}`
-} else {
+if (data.from === '' && data.from_who === '') from = '佚名'
+else if (data.from !== '') from = converter(data.from)
+else if (data.from_who !== '') from = converter(data.from_who)
+else {
   console.error('Unexpected error')
   Deno.exit(1)
 }
 
-const content = `${converter(data.hitokoto) + from}\n\n於 ${new Date().toLocaleString(
-  'zh-TW',
-  {
-    timeZone: 'Asia/Taipei',
-    hourCycle: 'h23'
-  }
-)} 更新\n`
+const content = `${converter(data.hitokoto)}
+ ——${from}
+
+於 ${new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei', hourCycle: 'h23' })} 更新`
 
 try {
   await Deno.mkdir(join(Deno.cwd(), 'dist'), { recursive: true })
